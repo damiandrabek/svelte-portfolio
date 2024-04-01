63d6d37119bf4ef27d638ea3dadc0b04f0cbf47c
damiandrabek <damiandrabek1703@gmail.com>
2024-04-01 23:01:42 +0200

    main component finalised

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,11 +15,27 @@
       icon: "fa-solid fa-table-tennis-paddle-ball",
     },
   ];
+
+  let benefits = [
+    {
+      name: "a self taught developer",
+      description:
+        "I taught my self using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences.",
+    },
+    {
+      name: "a product design and UX fanatic",
+      description:
+        "Carafully crafting and designing amazing UIs allows me to express and experiment with creativity I have",
+    },
+    {
+      name: "an excellent communicator",
+      description: "Communication is key and it's paramount value of mine.",
+    },
+  ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-  <section
-    id="introPage"
+  <section id="introPage"
     class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
   >
     <div
@@ -54,7 +70,8 @@
     </div>
   </section>
 
-  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+  <section id="projects" 
+    class="py-20 lg:py-32 flex flex-col gap-24">
     <div class="flex flex-col gap-2 text-center md:text-2xl">
       <h6 class="text-lg sm:text-xl">A few of my creative endeavors.</h6>
       <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
@@ -66,7 +83,7 @@
       target="_blank"
       class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
     >
-      <i class="fa-regular fa-circle-play"></i>
+      <i class="fa-regular fa-circle-play text-slate-500"></i>
       <p>Watch the video</p>
     </a>
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
@@ -95,5 +112,104 @@
     </div>
   </section>
 
-  <section id="about"></section>
+  <section id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">What to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-4xl">
+        A bit <span class="poppins text-violet-400 k">about</span> me.
+      </h3>
+    </div>
+    <p class="mx-auto poppins font-semibold text-lg sm:texl-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+          >
+            0{index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+    <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">The <span class="text-violet-500 ">Complete</span> Package</h5>
+    <div
+      class="flex flex-col overflow-x-scroll gap-10 max-x-[800px] mx-auto w-full"
+    >
+      <table class="bg-white text-slate-700 rounded text-center">
+        <thead class="border-b border-solid border-slate-200">
+          <tr>
+            <th />
+            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+            <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8"
+              >Me</th
+            >
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Dedication
+            </td>
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Critical thought
+            </td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Interpersonal skills
+            </td>
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Programming ability
+            </td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-green-500"></i></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+      <p>Scroll to see more &rarr;</p>
+    </div>
+    <p class="mx-auto">So why not invest?</p>
+  </section>
 </main>
